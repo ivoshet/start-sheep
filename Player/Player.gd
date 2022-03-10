8957f54dda6bd5b49fe7a05c13480df4aafc4805
ivoshet <ivan.v.shet@gmail.com>
2022-03-10 19:58:00 +0700

    added screen wrap

--- Player/Player.gd
+++ Player/Player.gd
@@ -20,17 +20,26 @@ func _ready():
 #	initial state - ALIVE
 #	print(typeof(state))
 	change_state(ALIVE)
+#	получаем размер видимого экрана
 	screensize = get_viewport().get_visible_rect().size
-	
-	
+
 func _process(delta):
 	get_input()
-	
-func _physics_process(delta):
-	set_applied_force(thrust.rotated(rotation))
-	set_applied_torque(spin_power * rotation_dir)
 
-# the finite state machine
+#	получаем вектор направления
+func get_input():
+	thrust = Vector2()
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = Vector2(engine_power, 0)
+	rotation_dir = 0
+	if Input.is_action_pressed("rotate_right"):
+		rotation_dir += 1
+	if Input.is_action_pressed('rotate_left'):
+		rotation_dir -= 1
+		
+# автомат конечных состояний
 func change_state(new_state):
 	match new_state:
 		INIT:
@@ -43,18 +52,31 @@ func change_state(new_state):
 			$CollisionShape2D.disabled = true
 	state = new_state
 
-func get_input():
-	thrust = Vector2()
-	if state in [DEAD, INIT]:
-		return
-	if Input.is_action_pressed("thrust"):
-		thrust = Vector2(engine_power, 0)
-	rotation_dir = 0
-	if Input.is_action_pressed("rotate_right"):
-		rotation_dir += 1
-	if Input.is_action_pressed('rotate_left'):
-		rotation_dir -= 1
-	
+#	метод прямого доступа к свойствам RigidBody
+#	применяется для получения положения объекта, 
+#	
+func _integrate_forces(physics_state):
+#	задаем физические силы действующие на объект
+	set_applied_force(thrust.rotated(rotation))
+	set_applied_torque(spin_power * rotation_dir)
+#	получаем текущее положение
+	var xform = physics_state.get_transform()
+	if xform.origin.x > screensize.x:
+		xform.origin.x = 0
+	if xform.origin.x < 0:
+		xform.origin.x = screensize.x
+	if xform.origin.y > screensize.y:
+		xform.origin.y = 0
+	if xform.origin.y < 0:
+		xform.origin.y = screensize.y
+#	задаем новое положение
+	physics_state.set_transform(xform)
+		
+
+
+
+
+
 		
 		
 		
