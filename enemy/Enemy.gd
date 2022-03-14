b7087cd985a5c9471d87fa3854706192c53a8540
ivoshet <ivan.v.shet@gmail.com>
2022-03-14 21:14:36 +0700

    added shooting for enemy

--- enemy/Enemy.gd
+++ enemy/Enemy.gd
@@ -28,7 +28,24 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
 
 func _on_GunTimer_timeout():
-	pass # Replace with function body.
+	shoot_pulse(3, 0.15)
 	
+func shoot():
+	var dir = target.global_position - global_position
+	dir = dir.rotated(rand_range(-0.1, 0.1)).angle()
+	emit_signal('shoot', Bullet, global_position, dir)
+
+func shoot_pulse(n, delay):
+	for i in range(n):
+		shoot()
+		yield(get_tree().create_timer(delay), 'timeout')
+
+func take_damage(amount):
+	pass
+	
+
+
+
+
 	
 	
